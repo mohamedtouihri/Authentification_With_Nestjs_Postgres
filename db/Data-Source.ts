5d49971062dfbba4c9e9006b0330400c6910cabc
Melek touihri <twihrimelek9@gmail.com>
2024-11-10 11:04:18 +0100

    AuthUpdate

--- db/Data-Source.ts
+++ db/Data-Source.ts
@@ -1,33 +1,17 @@
-// import { DataSource, DataSourceOptions } from "typeorm";
-// import {config} from 'dotenv';
-// import { ConfigModule, ConfigService } from "@nestjs/config";
-// import { TypeOrmModule } from "@nestjs/typeorm";
-// config();
-// export const DataSourceOption:DataSourceOptions = {
-//     import:[
-//         ConfigModule.forRoot({
-//           envFilePath: [`stage.${process.env.STAGE}.env`],
-//         }),
-//         TypeOrmModule.forRootAsync({
-//          imports: [ConfigModule],
-//          inject: [ConfigService],
-//          useFactory: async (configService: ConfigService) => {
-//            return {
-//               type: 'postgres',
-//               host: configService.get('DB_HOST'),
-//               port: configService.get('DB_PORT'),
-//               username: configService.get('DB_USERNAME'),
-//               password: configService.get('DB_PASSWORD'),
-//               database: configService.get('DB_DATABASE'),
-//               entities:['dist/**/*.entity{.ts,.js}'],
-//               migrations:['dist/db/migrations/*{.ts,.js}'],
-//               logging:false,
-//               synchronize:false
-//             };
-//          },
-//         }),
-//         ]
-// }
-
-// const dataSource = new DataSource(DataSourceOption)
-// export default dataSource;
+import { DataSource, DataSourceOptions } from "typeorm";
+import {config} from 'dotenv';
+config();
+export const DataSourceOption:DataSourceOptions = {
+    type: 'postgres',
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    entities:['dist/**/*.entity{.ts,.js}'],
+    migrations:['dist/db/migrations/*{.ts,.js}'],
+    logging:false,
+    synchronize:false
+};
+const dataSource = new DataSource(DataSourceOption)
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { DataSourceOption } from 'db/Data-Source';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: [`stage.${process.env.STAGE}.env`],
-    }),
-    TypeOrmModule.forRootAsync({
-     imports: [ConfigModule],
-     inject: [ConfigService],
-     useFactory: async (configService: ConfigService) => {
-       return {
-          type: 'postgres',
-          host: configService.get('DB_HOST'),
-          port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
-          entities:['dist/**/*.entity{.ts,.js}'],
-          migrations:['dist/db/migrations/*{.ts,.js}'],
-          logging:false,
-          synchronize:true
-        };
-     },
-    }),
+  imports: [TypeOrmModule.forRoot(DataSourceOption),
     UsersModule],
   controllers: [],
   providers: [],
