23f930217a69494ed0439c75b92f9c91801f6f0b
Melek touihri <twihrimelek9@gmail.com>
2024-12-08 16:01:09 +0100

    Categorie,Course and Review Completed

--- src/utility/guards/authorization.guard.ts
+++ src/utility/guards/authorization.guard.ts
@@ -1,22 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable, mixin, UnauthorizedException } from "@nestjs/common";
-import { Reflector } from "@nestjs/core";
-import { Observable } from "rxjs";
+import { CanActivate, ExecutionContext, mixin, UnauthorizedException } from "@nestjs/common";
 
-// @Injectable()
-// export class AuthorizeGuard implements CanActivate{
-
-//     constructor(private reflector:Reflector){}
-//     canActivate(context: ExecutionContext): boolean{
-//         const allowedRoles = this.reflector.get<string[]>('allowedRoles',context.getHandler());
-//         const request = context.switchToHttp().getRequest();
-//         const result = request?.currentUser?.roles.map((role:string)=>allowedRoles.includes(role)).
-//         find((val:boolean)=>val===true);
-//         if(result) return true;
-//         throw new UnauthorizedException('Sorry, you are not authorized.')
-        
-//     }
-
-// }
 
 export const  AuthorizeGuard=(allowedRoles:string[])=>{
     class RolesGuardMixin implements CanActivate {
