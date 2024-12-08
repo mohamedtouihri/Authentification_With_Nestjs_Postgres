d364a356c379729f09303c6d5f97b28d3d191b39
Melek touihri <twihrimelek9@gmail.com>
2024-12-08 15:31:56 +0100

    Completed Reviews

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -5,6 +5,9 @@ import { UpdateReviewDto } from './dto/update-review.dto';
 import { AuthenticationGuard } from 'src/utility/guards/authentication.guard';
 import { CurrentUser } from 'src/utility/decorators/current-user.decorator';
 import { UserEntity } from 'src/users/entities/user.entity';
+import { ReviewEntity } from './entities/review.entity';
+import { Roles } from 'src/utility/common/user-roles.enum';
+import { AuthorizeGuard } from 'src/utility/guards/authorization.guard';
 
 @Controller('reviews')
 export class ReviewsController {
@@ -12,18 +15,24 @@ export class ReviewsController {
   
   @UseGuards(AuthenticationGuard)
   @Post()
-  async create(@Body() createReviewDto: CreateReviewDto,@CurrentUser() currentUser:UserEntity) {
+  async create(@Body() createReviewDto: CreateReviewDto,@CurrentUser() currentUser:UserEntity)
+  :Promise<ReviewEntity> {
     return await this.reviewsService.create(createReviewDto,currentUser);
   }
 
-  @Get()
+  @Get('/all')
   findAll() {
     return this.reviewsService.findAll();
   }
 
+  @Get()
+  async findAllByCourse(@Body('courseId') courseId:number) {
+    return await this.reviewsService.findAllByCourse(+courseId)
+  }
+
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+  async findOne(@Param('id') id: string):Promise<ReviewEntity> {
+    return await this.reviewsService.findOne(+id);
   }
 
   @Patch(':id')
@@ -31,6 +40,7 @@ export class ReviewsController {
     return this.reviewsService.update(+id, updateReviewDto);
   }
 
+  @UseGuards(AuthenticationGuard,AuthorizeGuard([Roles.ADMIN]))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.reviewsService.remove(+id);
--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 import { UserEntity } from 'src/users/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ReviewEntity } from './entities/review.entity';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
 import { CoursesService } from 'src/courses/courses.service';
 
 @Injectable()
@@ -12,33 +12,59 @@ export class ReviewsService {
   constructor(@InjectRepository(ReviewEntity) 
   private readonly reviewRepository:Repository<ReviewEntity>,
   private readonly courseService:CoursesService){}
-  async create(createReviewDto: CreateReviewDto,currentUser:UserEntity) {
+  async create(createReviewDto: CreateReviewDto,currentUser:UserEntity):Promise<ReviewEntity>{
     const course = await this.courseService.findOne(createReviewDto.courseId);
     let review = await this.findOneByUserAndCourse(currentUser.id,createReviewDto.courseId);
-    if(!review){
-      review = this.reviewRepository.create(createReviewDto);
-      review.user = currentUser;
-      review.course = course;
+    const newReview: DeepPartial<ReviewEntity> = {
+      rating: createReviewDto.ratings,
+      user: currentUser,
+      course: course,
+    };
+    if (!review) {
+      review = this.reviewRepository.create(newReview);
     }else{
       review.rating=createReviewDto.ratings
     }
-    return createReviewDto;
+    return await this.reviewRepository.save(review);
   }
 
   findAll() {
     return `This action returns all reviews`;
   }
+  async findAllByCourse(id:number): Promise<ReviewEntity[]>{
+    const course = await this.courseService.findOne(id);
+    return await this.reviewRepository.find({
+      where:{course:{id}},
+      relations:{
+        user:true,
+        course:{
+          category:true
+        }
+      }
+    })
+  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
+  async findOne(id: number): Promise<ReviewEntity>{
+    const review = await this.reviewRepository.findOne({
+      where:{id},
+      relations:{
+        user:true,
+        course:{
+          category:true
+        }
+      }
+    })
+    if(!review) throw new NotFoundException('Review not found.');
+    return review;
   }
 
   update(id: number, updateReviewDto: UpdateReviewDto) {
     return `This action updates a #${id} review`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+  async remove(id: number) {
+    const review = await this.findOne(id);
+    return this.reviewRepository.remove(review);
   }
 
   async findOneByUserAndCourse(userId:number,courseId:number){
@@ -58,6 +84,5 @@ export class ReviewsService {
         }
       }
     })
-
   }
 }
